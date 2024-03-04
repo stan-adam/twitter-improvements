42b55065ce06721b4809b33c914e8a8d641920eb
Adam Stankiewicz <stan.adamm@gmail.com>
2024-03-04 20:08:09 +0000

    Implement video and gif saving, refactor methods

--- background.js
+++ background.js
@@ -55,10 +55,44 @@ async function download(url, filename) {
     });
 }
 
+async function download_cobalt(url, filename) {
+    let api_url = "https://co.wuk.sh/api/json";
+    let data = {
+        vQuality: "max",
+        filenamePattern: "nerdy",
+        twitterGif: true,
+        url: url
+    };
+    let requestOptions = {
+        method: 'POST',
+        headers: {
+            Accept: "application/json",
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(data)
+    };
+    let response = await fetch(api_url, requestOptions);
+    let json = await response.json();
+    let down_url = json.url;
+    if (down_url.includes(".mp4")) {
+        filename += ".mp4";
+    }
+    else {
+        filename += ".gif";
+    }
+    chrome.downloads.download({
+        filename: filename,
+        url: json.url
+    });
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if (request.thespecialsecret = "download") {
+        if (request.thespecialsecret === "download") {
             download(request.downurl, request.downfilename);
+        }
+        else if (request.thespecialsecret === "download_cobalt") {
+            download_cobalt(request.downurl, request.downfilename);
         }
     }
 )
--- twitter_improvements.js
+++ twitter_improvements.js
@@ -2,20 +2,24 @@ const ENABLE_LOGGING = false;
 const TWEET_FUNCTIONS = async (tweet) => {
     add_vx(tweet);
 };
-const MEDIA_FUNCTIONS = async (media) => {
-    save_media(media);
+const IMAGE_FUNCTIONS = async (image) => {
+    save_image(image);
 };
+const VIDEO_FUNCTIONS = async (video) => {
+    save_video(video);
+}
 
 let VX_BUTTON;
-let DOWNLOAD_BUTTON;
+let DOWNLOAD_BUTTON_IMAGE;
+let DOWNLOAD_BUTTON_VIDEO;
 
 // Functions to run on each tweet
 
 async function add_vx(tweet) {
     try {
-        let share_button = await getTweetButtonPositionAnchor(tweet);
+        let share_button = await get_tweet_anchor(tweet);
         let vx_button = VX_BUTTON.cloneNode(true);
-        let url = await getVX_URL(tweet);
+        let url = await get_tweet_url_vx(tweet);
         vx_button.onmousedown = () => navigator.clipboard.writeText(url);
         share_button.after(vx_button);
         ENABLE_LOGGING && console.log("Created VX Button: " + vx_button);
@@ -26,27 +30,38 @@ async function add_vx(tweet) {
     }
 }
 
-async function save_media(media) {
+async function save_image(image) {
     try {
-        let anchor = await getMediaButtonPositionAnchor(media);
-        let download_button = DOWNLOAD_BUTTON.cloneNode(true);
-        if (media.nodeName === "IMG") {
-            let url = await getOriginalImageUrl(media);
-            let filename = await getImageFileName(media);
-            anchor.appendChild(download_button);
-            download_button.onmousedown = () => {
-                chrome.runtime.sendMessage({thespecialsecret: "download", downurl: url, downfilename: filename});
-            };
-        }
-        else {
-            anchor.appendChild(download_button);
-        }
-
-        // ENABLE_LOGGING && console.log("Created Save Media Button: ");
+        let anchor = await get_image_anchor(image);
+        let download_button = DOWNLOAD_BUTTON_IMAGE.cloneNode(true);
+        let url = await get_image_full_res_url(image);
+        let filename = await get_image_filename(image);
+        anchor.appendChild(download_button);
+        download_button.onmousedown = () => chrome.runtime.sendMessage({thespecialsecret: "download", downurl: url, downfilename: filename});
+        ENABLE_LOGGING && console.log("Created Image Download Button: " + download_button);
     }
     catch(error) {
-        ENABLE_LOGGING && console.error("save_media error: " + error);
-        console.error("save_media error: " + error);
+        ENABLE_LOGGING && console.error("save_image error: " + error);
+        return;
+    }
+}
+
+async function save_video(video) {
+    try {
+        let anchor = await get_video_anchor(video);
+        let download_button = DOWNLOAD_BUTTON_VIDEO.cloneNode(true);
+        anchor.appendChild(download_button);
+        for(let i = 0; i < 16; i++) {
+            video = video.parentNode;
+        }
+        let filename = await get_video_filename(video);
+        let url = encodeURI(await get_tweet_url_normal(video));
+        download_button.onmousedown = () => chrome.runtime.sendMessage({thespecialsecret: "download_cobalt", downurl: url, downfilename: filename});
+        anchor.after(download_button);
+        ENABLE_LOGGING && console.log("Created Video Download Button: " + download_button);
+    }
+    catch(error) {
+        ENABLE_LOGGING && console.error("save_video error: " + error);
         return;
     }
 }
@@ -54,27 +69,21 @@ async function save_media(media) {
 
 // Button Functions
 
-async function getTweetButtonPositionAnchor(tweet) {
+async function get_tweet_anchor(tweet) {
     let share_button = tweet.querySelector('div[aria-label="Share post"]');
     if (share_button === null) return;
     return share_button.parentNode.parentNode;
 }
 
-async function getMediaButtonPositionAnchor(media) {
-    if(media.nodeName === "IMG") {
-        return media.parentNode; // add first child to this
-    }
-    else {
-        try {
-            return media.parentNode.parentNode.parentNode.childList[1].firstChild.firstChild.childList[3].firstChild.childList[1].childList[1];
-        }
-        catch {
-            return media.parentNode.parentNode.parentNode.childList[1].firstChild.firstChild;
-        }
-    }
+async function get_image_anchor(media) {
+    return media.parentNode; // add first child to this
 }
 
-async function createButton(svg_path) {
+async function get_video_anchor(video) {
+    return await get_tweet_anchor(media.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode);
+}
+
+async function get_button(svg_path) {
     let div = document.createElement("div");
     let clickable_div = document.createElement("div");
     let btn = document.createElement("button");
@@ -91,16 +100,25 @@ async function createButton(svg_path) {
     return div;
 }
 
-async function createVXButton() {
-    let div = await createButton("M 18.36 5.64 c -1.95 -1.96 -5.11 -1.96 -7.07 0 l -1.41 1.41 l -1.42 -1.41 l 1.42 -1.42 c 2.73 -2.73 7.16 -2.73 9.9 0 c 2.73 2.74 2.73 7.17 0 9.9 l -1.42 1.42 l -1.41 -1.42 l 1.41 -1.41 c 1.96 -1.96 1.96 -5.12 0 -7.07 z m -2.12 3.53 z m -12.02 0.71 l 1.42 -1.42 l 1.41 1.42 l -1.41 1.41 c -1.96 1.96 -1.96 5.12 0 7.07 c 1.95 1.96 5.11 1.96 7.07 0 l 1.41 -1.41 l 1.42 1.41 l -1.42 1.42 c -2.73 2.73 -7.16 2.73 -9.9 0 c -2.73 -2.74 -2.73 -7.17 0 -9.9 z m 1 5 l 1.2728 -1.2728 l 2.9698 1.2728 l -1.4142 -2.8284 l 1.2728 -1.2728 l 2.2627 6.2225 l -6.364 -2.1213 m 4.9497 -4.9497 l 3.182 1.0607 l 1.0607 3.182 l 1.2728 -1.2728 l -0.7071 -2.1213 l 2.1213 0.7071 l 1.2728 -1.2728 l -3.182 -1.0607 l -1.0607 -3.182 l -1.2728 1.2728 l 0.7071 2.1213 l -2.1213 -0.7071 l -1.2728 1.2728");
+async function get_vx_share_button() {
+    let div = await get_button("M 18.36 5.64 c -1.95 -1.96 -5.11 -1.96 -7.07 0 l -1.41 1.41 l -1.42 -1.41 l 1.42 -1.42 c 2.73 -2.73 7.16 -2.73 9.9 0 c 2.73 2.74 2.73 7.17 0 9.9 l -1.42 1.42 l -1.41 -1.42 l 1.41 -1.41 c 1.96 -1.96 1.96 -5.12 0 -7.07 z m -2.12 3.53 z m -12.02 0.71 l 1.42 -1.42 l 1.41 1.42 l -1.41 1.41 c -1.96 1.96 -1.96 5.12 0 7.07 c 1.95 1.96 5.11 1.96 7.07 0 l 1.41 -1.41 l 1.42 1.41 l -1.42 1.42 c -2.73 2.73 -7.16 2.73 -9.9 0 c -2.73 -2.74 -2.73 -7.17 0 -9.9 z m 1 5 l 1.2728 -1.2728 l 2.9698 1.2728 l -1.4142 -2.8284 l 1.2728 -1.2728 l 2.2627 6.2225 l -6.364 -2.1213 m 4.9497 -4.9497 l 3.182 1.0607 l 1.0607 3.182 l 1.2728 -1.2728 l -0.7071 -2.1213 l 2.1213 0.7071 l 1.2728 -1.2728 l -3.182 -1.0607 l -1.0607 -3.182 l -1.2728 1.2728 l 0.7071 2.1213 l -2.1213 -0.7071 l -1.2728 1.2728");
+    return await add_share_bar_classes_to_div(div);
+}
+
+async function get_video_download_button() {
+    let div = await get_button("M 12 17.41 l -5.7 -5.7 l 1.41 -1.42 L 11 13.59 V 4 h 2 V 13.59 l 3.3 -3.3 l 1.41 1.42 L 12 17.41 zM21 15l-.02 3.51c0 1.38-1.12 2.49-2.5 2.49H5.5C4.11 21 3 19.88 3 18.5V15h2v3.5c0 .28.22.5.5.5h12.98c.28 0 .5-.22.5-.5L19 15h2z");
+    return await add_share_bar_classes_to_div(div);
+}
+
+async function add_share_bar_classes_to_div(div) {
     div.classList.add("usybuttondiv");
     div.firstChild.classList.add("usybuttonclickdiv");
     div.firstChild.firstChild.classList.add("usybutton");
     return div;
 }
 
-async function createDownloadButton() {
-    let div = await createButton("M 12 17.41 l -5.7 -5.7 l 1.41 -1.42 L 11 13.59 V 4 h 2 V 13.59 l 3.3 -3.3 l 1.41 1.42 L 12 17.41 zM21 15l-.02 3.51c0 1.38-1.12 2.49-2.5 2.49H5.5C4.11 21 3 19.88 3 18.5V15h2v3.5c0 .28.22.5.5.5h12.98c.28 0 .5-.22.5-.5L19 15h2z");
+async function get_image_download_button() {
+    let div = await get_button("M 12 17.41 l -5.7 -5.7 l 1.41 -1.42 L 11 13.59 V 4 h 2 V 13.59 l 3.3 -3.3 l 1.41 1.42 L 12 17.41 zM21 15l-.02 3.51c0 1.38-1.12 2.49-2.5 2.49H5.5C4.11 21 3 19.88 3 18.5V15h2v3.5c0 .28.22.5.5.5h12.98c.28 0 .5-.22.5-.5L19 15h2z");
     div.classList.add("usybuttondiv", "usyimagediv");
     div.firstChild.classList.add("usybuttonclickdiv");
     div.firstChild.firstChild.classList.add("usybutton", "usydownloadbutton");
@@ -110,7 +128,7 @@ async function createDownloadButton() {
 
 // Helper functions
 
-async function splitLinkAsVX(split_url) {
+async function get_tweet_url_as_vx_from_split(split_url) {
     let vx_url = split_url[0];
     if (split_url[2] === "twitter.com") {
         vx_url += "//vx";
@@ -124,7 +142,15 @@ async function splitLinkAsVX(split_url) {
     return vx_url;
 }
 
-async function get_split_tweet_url(tweet) {
+async function split_link_to_normal(split_url) {
+    let url = split_url[0] + "//";
+    for(let i = 2; i < 6; i++) {
+        url += split_url[i] + "/";
+    }
+    return url;
+}
+
+async function get_tweet_url_split(tweet) {
     let url = Array.from(tweet.querySelectorAll('a'))
                    .map(link => link.href.split("/"))
                    .filter(link => link[2] === "twitter.com" || link[2] === "x.com")
@@ -132,16 +158,21 @@ async function get_split_tweet_url(tweet) {
     return url[0];
 }
 
-async function getVX_URL(tweet) {
-    let url = await get_split_tweet_url(tweet);
-    return await splitLinkAsVX(url);
+async function get_tweet_url_normal(tweet) {
+    let url = await get_tweet_url_split(tweet);
+    return await split_link_to_normal(url);
 }
 
-async function getOriginalImageUrl(media) {
+async function get_tweet_url_vx(tweet) {
+    let url = await get_tweet_url_split(tweet);
+    return await get_tweet_url_as_vx_from_split(url);
+}
+
+async function get_image_full_res_url(media) {
     return media.src.replace(/name=[^&]*/, "name=orig");
 }
 
-async function getImageFileName(media) {
+async function get_image_filename(media) {
     let url = window.location.href.split("/")[6];
     if (url === "photo") {
         url = window.location.href.split("/");
@@ -157,33 +188,33 @@ async function getImageFileName(media) {
     return "[twitter] " + user + " - " + id + " - " + num + "." + file_end;
 }
 
+async function get_video_filename(tweet) {
+    let url = await get_tweet_url_split(tweet);
+    let user = url[3];
+    let id = url[5];
+    return "[twitter] " + user + " - " + id + " - VID";
+}
+
 
 // Observes new tweets, runs each function on them
 
 async function init_buttons() {
-    VX_BUTTON = await createVXButton();
-    DOWNLOAD_BUTTON = await createDownloadButton();
+    VX_BUTTON = await get_vx_share_button();
+    DOWNLOAD_BUTTON_IMAGE = await get_image_download_button();
+    DOWNLOAD_BUTTON_VIDEO = await get_video_download_button();
 }
 
-async function getImageNodes(nodes) {
+async function get_image_nodes(nodes) {
     return nodes.filter(node => node.nodeName === "IMG")
                 .filter(node => node.src.includes("https://pbs.twimg.com/media/"));
 }
 
-async function getVideoNodes(nodes) {
-    // return nodes.filter(node => node.nodeName === "DIV")
-    //             .filter(node => node.querySelector('video[poster*="https://pbs.twimg.com/ext_tw_video_thumb/"]') || node.querySelector('video[poster*="https://pbs.twimg.com/amplify_video_thumb/"]'));
-    
-    // return nodes.filter(node => node.nodeName === "DIV")
-    //             .filter(node => node.querySelector('div[data-testid="videoComponent"]'));
-
-    // return nodes.filter(node => node.nodeName === "VIDEO");
-
+async function get_video_nodes(nodes) {  
     return nodes.filter(node => node.nodeName === "DIV")
-                .filter(node => node.querySelector('div[aria-label="Video Settings"]'));
+                .filter(node => node.querySelector('div[data-testid="videoComponent"]'));
 }
 
-async function getTweetNodes(nodes) {
+async function get_tweet_nodes(nodes) {
     return nodes.filter(node => node.nodeName === "DIV")
                 .filter(node => node.attributes.item(0) !== null)
                 .filter(node => node.attributes.item(0).nodeValue === 'cellInnerDiv');
@@ -197,9 +228,9 @@ async function tweet_observer() {
                                 .filter(nodelist => nodelist.length > 0)
                                 .map(nodelist => nodelist[0]);
         
-        getTweetNodes(nodes).then(nodes => nodes.forEach(node => TWEET_FUNCTIONS(node)));
-        getImageNodes(nodes).then(nodes => nodes.forEach(node => MEDIA_FUNCTIONS(node)));
-        // getVideoNodes(nodes).then(nodes => nodes.forEach(node => MEDIA_FUNCTIONS(node)));
+        get_tweet_nodes(nodes).then(nodes => nodes.forEach(node => TWEET_FUNCTIONS(node)));
+        get_image_nodes(nodes).then(nodes => nodes.forEach(node => IMAGE_FUNCTIONS(node)));
+        get_video_nodes(nodes).then(nodes => nodes.forEach(node => VIDEO_FUNCTIONS(node)));
     }
 
     const observerConfig = {
